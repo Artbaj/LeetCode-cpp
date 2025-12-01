88b1b563288aaa8125b8c1244f387ebef9f825ca
Maciej Śmigórski <98353003+Artbaj@users.noreply.github.com>
2025-12-01 18:01:02 +0100

    Time: 4 ms (37.57%), Space: 9.8 MB (25.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> map;
         if(s.length()!=t.length()) return false;
-        else{
-            for(int i=0;i<t.length();i++){
+        unordered_map<char,int> map;
+       
+        for(int i=0;i<t.length();i++){
                 map[t.at(i)]++;
                 map[s.at(i)]--;
             }
-        }
         for(auto p:map){
             if(p.second!=0) return false;
         }
